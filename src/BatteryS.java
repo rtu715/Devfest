2edcfb662cece9712e50ae1013b7dbbe3fcc8d6a
Renbo <rt2632@columbia.edu>
2017-02-04 04:33:16 -0500

    Lillian's stuff

---
+++ src/BatteryS.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class BatteryS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Kernel132.SYSTEM_POWER_STATUS batteryStatus = new Kernel132.SYSTEM_POWER_STATUS();
+		Kernel132.INSTANCE.GetSystemPowerStatus(batteryStatus);
+
+		System.out.println(batteryStatus);
+
+	}
+
+}
